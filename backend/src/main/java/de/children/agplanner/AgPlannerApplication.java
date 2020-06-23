8e2e8fbf3441200e0ed85c2746af3b04c0c5a6dc
Your Name <not@viadee.de>
2020-06-23 19:17:30 +0200

    Initial working backend

---
+++ backend/src/main/java/de/children/agplanner/AgPlannerApplication.java
@@ -0,0 +1,13 @@
+package de.children.agplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgPlannerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/children/agplanner/AgPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.children.agplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
